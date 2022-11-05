ed33b4efd34e3cb38a942173a50d3935fd897d0e
Olarewaju Victor <ayomidevictor271@gmail.com>
2022-11-05 18:53:21 +0100

    new

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const postRoute = require('./routes/postRoute')
 require('dotenv').config()
 
 
-const PORT = process.env.PORT ||5000
+const PORT = process.env.PORT || 5000
 
 const app = express()
 Dbconnect()
